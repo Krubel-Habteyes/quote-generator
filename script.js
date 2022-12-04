2f91a43aedbe1f1064436433e88676b756e01432
Krubel Sahle Habteyes <habteyes83@gmail.com>
2022-12-04 09:06:13 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+let apiQuotes = [];
 
-console.log('testing');
+
+//Get quotes from API
+async function getQuotes() {
+	const apiUrl = "https://type.fit/api/quotes";
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+        console.log(apiQuotes[12]);
+	} catch (error) {}
+}
+
+
+//On load
+getQuotes();
